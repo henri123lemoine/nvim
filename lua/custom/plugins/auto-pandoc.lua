5de30be591fdfd3bd47e75b6177cdf2f333df21e
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-09 01:43:46 -0400

    Added harpoon and markdown.nvim to config

--- lua/custom/plugins/auto-pandoc.lua
+++ lua/custom/plugins/auto-pandoc.lua
@@ -1,3 +1,4 @@
+-- auto-pandoc.nvim
 return {
   'jghauser/auto-pandoc.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,45 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- markdown.nvim
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    main = 'render-markdown',
+    opts = {},
+    name = 'render-markdown',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
+
+  -- Markdown preview
+  {
+    -- Install markdown preview, use npx if available.
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function(plugin)
+      if vim.fn.executable 'npx' then
+        vim.cmd('!cd ' .. plugin.dir .. ' && cd app && npx --yes yarn install')
+      else
+        vim.cmd [[Lazy load markdown-preview.nvim]]
+        vim.fn['mkdp#util#install']()
+      end
+    end,
+    init = function()
+      if vim.fn.executable 'npx' then
+        vim.g.mkdp_filetypes = { 'markdown' }
+      end
+    end,
+  },
+  --
+  --   {
+  --     'iamcco/markdown-preview.nvim',
+  --     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  --     ft = { 'markdown' },
+  --     build = function()
+  --       vim.fn['mkdp#util#install']()
+  --     end,
+  --   },
+}
