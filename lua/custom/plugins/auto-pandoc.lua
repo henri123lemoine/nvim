5cdcd1326318c8281d70f5b8ce6a48a95afb542d
Henri Lemoine <henri123lemoine@gmail.com>
2024-09-08 20:19:35 -0400

    Added auto-pandoc

---
+++ lua/custom/plugins/auto-pandoc.lua
@@ -0,0 +1,63 @@
+return {
+  'jghauser/auto-pandoc.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  ft = 'markdown',
+  config = function()
+    local auto_pandoc = require 'auto-pandoc'
+
+    -- Function to run the custom pandoc command with improved error reporting
+    _G.RunCustomPandoc = function()
+      local input_file = vim.fn.expand '%:p'
+      local output_file = vim.fn.fnamemodify(input_file, ':r') .. '.pdf'
+      local cmd = string.format('pandoc %s -o %s --pdf-engine=xelatex', input_file, output_file)
+
+      vim.fn.jobstart(cmd, {
+        on_stdout = function(_, data)
+          if data then
+            vim.schedule(function()
+              vim.notify(table.concat(data, '\n'), vim.log.levels.INFO)
+            end)
+          end
+        end,
+        on_stderr = function(_, data)
+          if data then
+            vim.schedule(function()
+              vim.notify(table.concat(data, '\n'), vim.log.levels.ERROR)
+            end)
+          end
+        end,
+        on_exit = function(_, exit_code)
+          if exit_code == 0 then
+            vim.schedule(function()
+              vim.notify('PDF generated successfully: ' .. output_file, vim.log.levels.INFO)
+            end)
+          else
+            vim.schedule(function()
+              vim.notify('Failed to generate PDF. Exit code: ' .. exit_code, vim.log.levels.ERROR)
+            end)
+          end
+        end,
+      })
+    end
+
+    -- Create an autocommand group
+    local augroup = vim.api.nvim_create_augroup('AutoPandoc', { clear = true })
+
+    -- Set up keymaps for markdown files
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'markdown',
+      group = augroup,
+      callback = function()
+        -- Map 'go' to run auto-pandoc
+        vim.keymap.set('n', 'go', function()
+          auto_pandoc.run_pandoc()
+        end, { buffer = true, desc = 'Run auto-pandoc' })
+
+        -- Map 'gO' (shift+g, shift+o) to run the custom pandoc command
+        vim.keymap.set('n', 'gO', function()
+          RunCustomPandoc()
+        end, { buffer = true, desc = 'Run custom pandoc command' })
+      end,
+    })
+  end,
+}
