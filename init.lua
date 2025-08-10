a4d12a434a9adeb542c548cb59fb6f95e280dee1
Henri Lemoine <henri123lemoine@gmail.com>
2025-08-10 16:23:12 -0400

    Update .gitignore and fix ruff configuration

--- init.lua
+++ init.lua
@@ -535,30 +535,26 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         gopls = {},
-        pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        rust_analyzer = {},
+        pyright = {
+          settings = {
+            python = {
+              venvPath = '.',
+              venv = '.venv',
+            },
+          },
+        },
+        ruff = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
           -- capabilities = {},
           settings = {
             Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              completion = { callSnippet = 'Replace' },
+              diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
@@ -580,6 +576,8 @@ require('lazy').setup({
         'typescript-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+      require('lspconfig').pyright.setup(servers.pyright)
 
       require('lspconfig.configs').tsserver = {
         default_config = {
